72ebef1f28d0498d86299ccc76a361466378c289
Robert Guss <robertguss@pm.me>
2021-10-11 09:34:51 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creek - a blog theme for Astro
+
+Creek is an open source blog theme for [Astro SSG](https://docs.astro.build/getting-started/). This theme is built upon a theme caled [Starter's Creek](https://github.com/statamic/starter-kit-starters-creek) for [Statamic CMS](https://statamic.com/).
+
+This theme also take's some ideas and functionality from [Ink](https://github.com/one-aalam/astro-ink) another great theme for Astro.
---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
+  // Enable the Preact renderer to support Preact JSX components.
+  renderers: ["@astrojs/renderer-preact"],
+  devOptions: {
+    tailwindConfig: "./tailwind.config.js",
+  },
+  buildOptions: {
+    site: "https://yourdomain.com/",
+    sitemap: true,
+  },
+});
