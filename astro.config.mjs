e2bb965735d0461c3278e33a72097ed2e640ca07
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2023-11-05 05:08:58 +0000

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
- 
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
-    speedInsights:{
-      enabled: true,
-    },
-  })
-})
+    speedInsights: {
+      enabled: true
+    }
+  }),
+  integrations: [sanity(), react()]
+});
